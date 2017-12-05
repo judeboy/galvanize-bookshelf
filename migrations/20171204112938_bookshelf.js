ed7f6df14890f08fa8d9fd9b0ab6e3f76a534889
Judah Martin Trimmer <jutr4020@colorado.edu>
2017-12-04 17:25:36 -0700

    working on heroku

--- migrations/20171204112938_bookshelf.js
+++ migrations/20171204112938_bookshelf.js
@@ -7,7 +7,8 @@ exports.up = function(knex, Promise) {
   table.string('genre').notNullable().defaultsTo('')
   table.text('description').notNullable().defaultsTo('')
   table.text('cover_url').notNullable().defaultsTo('')
-  table.timestamps(true, true)
+  table.timestamp('created_at').notNullable().defaultsTo(knex.raw('now()'))
+  table.timestamp('updated_at').notNullable().defaultsTo(knex.raw('now()'))
 })
 };
 
--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,68 @@
 'use strict';
 
 const express = require('express');
-
+const knex = require('../knex.js')
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/books', (req,res,next) => {
+  return knex('books')
+  .select('id', 'title', 'author', 'genre', 'description', 'cover_url AS coverUrl','created_at AS createdAt', 'updated_at AS updatedAt')
+  .orderBy('title', 'asc')
+  .then((data) =>{
+    res.status(200).send(data)
+  })
+})
+
+router.get('/books/:id', (req,res,next) => {
+  let bookId = req.params.id
+  return knex('books')
+  .select('id', 'title', 'author', 'genre', 'description', 'cover_url AS coverUrl','created_at AS createdAt', 'updated_at AS updatedAt')
+  .where('id', bookId)
+  .then((data) =>{
+    res.status(200).send(data[0])
+  })
+})
+
+router.post('/books', (req,res,next) => {
+  return knex('books')
+  .insert({
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+    description: req.body.description,
+    cover_url: req.body.coverUrl
+  })
+  .returning(['title', 'author', 'genre', 'description', 'cover_url AS coverUrl', 'id'])
+  .then((data) => {
+    res.status(200).send(data[0])
+  })
+})
+
+router.patch('/books/:id', (req,res,next) => {
+  return knex('books')
+  .update({
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+    description: req.body.description,
+    cover_url: req.body.coverUrl
+  })
+  .returning(['title', 'author', 'genre', 'description', 'cover_url AS coverUrl', 'id'])
+  .then((data) => {
+    res.status(200).send(data[0])
+  })
+})
+
+router.delete('/books/:id', (req, res, next) => {
+  let bookId = req.params.id
+  return knex('books')
+  .where('id', bookId)
+  .del()
+  .first(['title', 'author', 'genre', 'description', 'cover_url AS coverUrl'])
+  .then((data) => {
+    res.status(200).send(data)
+  })
+})
 
 module.exports = router;
